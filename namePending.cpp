c4de68b809b738cbf3509a94b8fa6aa223c50fa5
ysSteve46 <yeetprimo645@gmail.com>
2024-07-23 14:37:40 +0800

    Update namePending.cpp

--- namePending.cpp
+++ namePending.cpp
@@ -4,18 +4,18 @@
 using namespace std;
 
 int main(){
-  int arr[10][2] = {{1,4,5,6,8,9,2,3,11,7},{22,3,10,24,15,17,5,20,1,30}};
+  int arr[2][10] = {{1,4,5,6,8,9,2,3,11,7},{22,3,10,24,15,17,5,20,1,30}};
   int month,date,mdiff,daydiff,totaldiff=99,person=0;
   cin >> month >> date;
   for(int i=0; i<10; i++){
-    mdiff=abs(month-arr[i][0]);
-    daydiff=abs(day-arr[i][1]);
+    mdiff=abs(month-arr[0][i]);
+    daydiff=abs(date-arr[1][i]);
     if(totaldiff>(mdiff+daydiff)){
       totaldiff=mdiff+daydiff;
       person=i;
     }
   }
-  cout <<　"The person's birthday close to this date is person " << person << '\n';
+  cout <<　"The person's birthday close to this date is person " << person+1 << '\n';
 
   return 0;
 }
